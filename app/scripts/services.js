22c3705131a3f88fdc17e9989ff139c68d409350
as7428 <as7428@student.uni-lj.si>
2014-04-17 00:38:52 +0200

    Added services.js

---
+++ app/scripts/services.js
@@ -0,0 +1,9 @@
+var skrollsServices = angular.module('skrollsServices', ['ngResource']);
+
+skrollsServices.factory('Skroll', ['$resource', 
+	function($resource){
+		return $resource('data/skrolls/s:skrollIDh.json', {}, {
+			query: {method:'GET', params:{skrollID:'phones'}, isArray:true}
+		});
+	}
+]);
