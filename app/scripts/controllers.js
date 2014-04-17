4e15761009a8a96b78c992f99442a06211840c84
as7428 <as7428@student.uni-lj.si>
2014-04-17 20:34:14 +0200

    Added services for REST

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,16 +2,10 @@
 
 var skrollControllers = angular.module('skrollControllers', []);
 
-skrollControllers.controller('SkrollListController', ['$scope', '$http',
-    function ($scope, $http) {
-        $http.get('data/skrolls/skrolls.json').success(function(data) {
-            $scope.skrolls = data;
-        });
-        $scope.sortBy = 'name';
-    }]);
-skrollControllers.controller('SkrollController', ['$scope', '$routeParams', '$http',
-    function ($scope, $routeParams, $http) {
-        $http.get('data/skrolls/s' + $routeParams.skrollID + '.json').success(function(data) {
-			$scope.posts = data;
-		});
+skrollControllers.controller('SkrollListController', ['$scope', 'Skroll', function($scope, Skroll) {
+  $scope.skrolls = Skroll.query();
+  $scope.sortBy = 'name';
+}]);
+skrollControllers.controller('SkrollController', ['$scope', '$routeParams', 'Skroll', function($scope, $routeParams, Skroll) {
+        $scope.posts = Skroll.get({skrollID: 's' + $routeParams.skrollID}, function(){});
     }]);
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -4,8 +4,7 @@ var skrollsServices = angular.module('skrollsServices', ['ngResource']);
 
 skrollsServices.factory('Skroll', ['$resource', 
 	function($resource){
-		return $resource('data/skrolls/s:skrollIDh.json', {}, {
+		return $resource('data/skrolls/:skrollID.json', {}, {
 			query: {method:'GET', params:{skrollID:'skrolls'}, isArray:true}
 		});
-	}
-]);
+	}]);
