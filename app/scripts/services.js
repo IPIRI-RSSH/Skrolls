983cd4116fe25ed9b7651ea436a2a7322f4dfd3f
as7428 <as7428@student.uni-lj.si>
2014-04-17 21:42:40 +0200

    Fixed controller, services...

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -7,16 +7,11 @@ skrollsServices.factory('Skroll', ['$resource',
 		return $resource('data/skrolls/:skrollID.json', {}, {
 			query: {method:'GET', params:{skrollID:'skrolls'}, isArray:true}
 		});
-<<<<<<< HEAD
-	}
-]);
+	}]);
 
-userServices.factory('User', ['$resource',
+skrollsServices.factory('User', ['$resource',
   function($resource){
-    return $resource('data/users/u:userID.json', {}, {
+    return $resource('data/users/:userID.json', {}, {
       query: {method:'GET', params:{userID:'user'}, isArray:false}
     });
   }]);
-=======
-	}]);
->>>>>>> as11712-master
