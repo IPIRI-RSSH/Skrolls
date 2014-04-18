923b1405963fe206a5019a228d4497ee6ee9cc1e
ne0n666 <darthnordon@gmail.com>
2014-04-18 13:25:07 +0200

    url finally working

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,14 +2,13 @@
 
 var skrollControllers = angular.module('skrollControllers', []);
 
-skrollControllers.controller('UserController', ['$scope', '$routeParams', 'User', function ($scope, $routeParams, User) {
-		$scope.user = User.get({userID: 'u' + $routeParams.userID}, function() {});
-		
+skrollControllers.controller('SkrollListController', ['$scope', 'SkrollFact', function($scope, SkrollFact) {
+	$scope.skrolls = SkrollFact.query();
+	$scope.sortBy = 'name';
 }]);
-skrollControllers.controller('SkrollListController', ['$scope', 'Skroll', function($scope, Skroll) {
-  $scope.skrolls = Skroll.query();
-  $scope.sortBy = 'name';
+skrollControllers.controller('SkrollController', ['$scope', '$routeParams', 'SkrollFact', function($scope, $routeParams, SkrollFact) {
+    $scope.posts = SkrollFact.get({skrollID: 's' + $routeParams.skrollID}, function(){});
 }]);
-skrollControllers.controller('SkrollController', ['$scope', '$routeParams', 'Skroll', function($scope, $routeParams, Skroll) {
-        $scope.posts = Skroll.get({skrollID: 's' + $routeParams.skrollID}, function(){});
+skrollControllers.controller('UserController', ['$scope', '$routeParams', 'UserFact', function ($scope, $routeParams, UserFact) {
+	$scope.user = UserFact.get({userID: 'u' + $routeParams.userID}, function() {});
 }]);
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,14 +2,14 @@
 
 var skrollsServices = angular.module('skrollsServices', ['ngResource']);
 
-skrollsServices.factory('Skroll', ['$resource', 
+skrollsServices.factory('SkrollFact', ['$resource', 
 	function($resource){
 		return $resource('data/skrolls/:skrollID.json', {}, {
 			query: {method:'GET', params:{skrollID:'skrolls'}, isArray:true}
 		});
 	}]);
 
-skrollsServices.factory('User', ['$resource',
+skrollsServices.factory('UserFact', ['$resource',
   function($resource){
     return $resource('data/users/:userID.json', {}, {
       query: {method:'GET', params:{userID:'user'}, isArray:false}
