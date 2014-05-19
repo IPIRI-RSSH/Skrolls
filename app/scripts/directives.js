99ce0e818f247a3a57624ff08f3b9304dc37b885
ne0n666 <darthnordon@gmail.com>
2014-05-19 16:35:21 +0200

    added some directives, login functionality

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -7,4 +7,17 @@ angular.module('skrollsDirectives', [])
 		restrict: 'A',
 		template: '<a class="button-skroll" href="/#/s/{{skroll.skrollID}}">{{skroll.name}} <span class="right">{{skroll.mark}}</span>'
 	};
+})
+.directive('changeName', function() {
+	return {
+		restrict: 'A',
+		template: '<span class="boldtext1">Display name: </span><input class="textfield-name" ng-model="displayname.username" type="text"></input>'
+	};
+})
+.directive('enterSkroll', function() {
+	return {
+		restrict: 'A',
+		template: '<h3> Enter Skroll name: </h3><form><input class="textfield" ng-model="skrollname" type="text" placeholder="new Skroll"/><br><a class="button" href="/#/s/{{skrollname}}">Open</a><br><br></form>'
+	};
 });
+
