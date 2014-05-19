8c8a12cd1fa85726301643dee520698c1a28f3c7
ne0n666 <darthnordon@gmail.com>
2014-05-19 23:44:00 +0200

    working on file upload

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -56,6 +56,7 @@ angular.module('skrollControllers', ['ngAnimate','ngResource','ngRoute'])
 			$scope.posting=false;
 		}
    	});
+   	$scope.imglink='';
 	$scope.message='';
 	$scope.url=document.URL;
 	$scope.posts=$scope.skroll.$child('posts');
@@ -63,11 +64,44 @@ angular.module('skrollControllers', ['ngAnimate','ngResource','ngRoute'])
 	$scope.displayname=nameFact.username;
 	$scope.post= function(){
 		var count=$scope.skroll.head['postCount'];
-		$scope.posts[count] = {text: $scope.message, author: $scope.displayname, timestamp: Firebase.ServerValue.TIMESTAMP};
+		$scope.posts[count] = {text: $scope.message, author: $scope.displayname.username, timestamp: Firebase.ServerValue.TIMESTAMP};
 		$scope.posts.$save(count);
 		count++;
 		$scope.skroll.$child('head').$update({postCount: count});
 	}
+
+	$scope.upload_image = function (image) {
+		if (!image.valid) return;
+
+		var imagesRef, safename, imageUpload;
+		
+		image.isUploading = true;
+		imageUpload = {
+			isUploading: true,
+			data: image.data,
+			name: image.filename
+		};
+		imagesRef = new Firebase('https://skrollsapp.firebaseio.com'+ '/images');
+
+		imagesRef.add(imageUpload).then(function(ref){
+			imagesRef.child(ref.name).child('isUploading').remove();
+			$scope.imglink=ref.name;
+			post();
+		});
+	
+				/*$scope.$apply(function () {
+					console.log("uploaded");
+					if ($scope.uploaded_callback !== undefined) {
+						$scope.uploaded_callback(angular.copy(imageUpload));
+					}
+					image.isUploading = false;
+					image.data = undefined;
+					image.filename = undefined;
+				});*/
+		
+		
+	};
+	
 
 }])
 .controller('UserController', ['$scope', '$routeParams', '$firebase', 'UserFact', function($scope, $routeParams, $firebase, UserFact) {
